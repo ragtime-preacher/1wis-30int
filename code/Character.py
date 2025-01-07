78a695fd7d7392b962c8a653f662db582b5096e2
Me <bryan.loertscher@missionary.org>
2025-01-06 23:04:10 -0600

    update some documentation

--- code/Character.py
+++ code/Character.py
@@ -3,13 +3,18 @@
 # In theory, contains everything that a character needs to have in order
 #   to be played in Dnd.
 
+# The actual character data will be stored in a dictionary that we can
+#   save to and load from a .json file.
+
 import json
 
 class Character:
     def __init__(self, filename: str):
-        self.data_file = open(filename)
-        self.data = json.loads(self.data_file)
+        self.data_file = open (filename)
+        self.data = json.load(self.data_file)
         return
+    
+    # TODO implement save()
     
     # Close the file
     def __del__(self):
