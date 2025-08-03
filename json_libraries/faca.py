0d9efd3d6c2c40f83b6917dfc34e558670ce6079
bryan <bryan.lurch@gmail.com>
2025-08-03 13:06:55 -0500

    implement spell and language filtering

--- json_libraries/faca.py
+++ json_libraries/faca.py
@@ -2,16 +2,19 @@
 # (FACA, or 'knife' in portuguese)
 
 from easygui import integerbox, choicebox
+import json
 
 def choose_age (flavor: str) :
     chosen_age = integerbox(flavor, default=25, lowerbound=0)
     return chosen_age
 
-def choose_language (flavor: str) :
-    chosen_language = choicebox(
-        msg=flavor,
-        title="Language selection",
-        choices=[
+def choose_language (flavor: str, ignore: list[str] | str = "Common") :
+    if type(ignore) == list:
+        ignore_list = [lang_to_ignore.casefold() for lang_to_ignore in ignore]
+    elif type(ignore) == str:
+        ignore_list = [ignore.casefold()]
+
+    lang_list = [
             # standard languages
             "Common",
             "Dwarvish",
@@ -31,14 +34,66 @@ def choose_language (flavor: str) :
             "Sylvan",
             "Undercommon"
         ]
+    chosen_language = choicebox(
+        msg=flavor,
+        title="Language selection",
+        choices=[lang for lang in lang_list if lang.casefold() not in ignore_list]
     )
     return chosen_language
 
-# TODO
+# TODO choose n spells with a multibox, allow for multiple spell_lists
+#   TODO (maybe) show only spells from certain schools of magic
 # spell_list defaults to wizard. Argue one or more
 #   levels of spells (0 = cantrip) to narrow the choices.
 def choose_spell (levels: list[int] | int, spell_list: str, flavor: str):
-    return "eldritch heckin' blast"
+    chosen_spell = choicebox(
+        msg=flavor,
+        title=f"spell selection: {spell_list}",
+        choices=spell_filter(levels, spell_list)
+    )
+    return chosen_spell
+
+# internal use only
+# argue 0 for cantrips.
+def spell_filter (spell_levels: list[int] | int, spell_list: str) -> list[str]:
+    # this dictionary is to help me interface with the spell library's format
+    json_ordinals = {
+        0: "Cantrip",
+        1: "1st-level",
+        2: "2nd-level",
+        3: "3rd-level",
+        4: "4th-level",
+        5: "5th-level",
+        6: "6th-level",
+        7: "7th-level",
+        8: "8th-level",
+        9: "9th-level"
+    }
+    spell_library_filename = "/home/feijao/programming/1wis-30int/json_libraries/efficient_spell_library.json"
+    spell_library = open(spell_library_filename)
+    spell_dict = json.load(spell_library)
+    spell_library.close()
+    found_spells = []
+    for i_spell in spell_dict.keys():
+        # first filter out by spell list
+        if spell_list.casefold() not in spell_dict[i_spell]["class"].casefold():
+            # this spell isn't on our spell list.
+            continue # on to the next one
+        if type(spell_levels) == int:
+            if spell_dict[i_spell]["level"] != json_ordinals[spell_levels]:
+                # this isn't the right level.
+                continue
+        elif type(spell_levels) == list:
+            if spell_dict[i_spell]["level"] not in [json_ordinals[i_level] for i_level in spell_levels]:
+                # not one of the right levels.
+                continue
+        # if we made it this far, the spell must line up with our criteria.
+        found_spells.append(i_spell)
+    if len(found_spells) == 0:
+        # for some reason, we didn't find anything. We'll add truly
+        #   the most devestating of spells
+        found_spells.append("FILTER ERROR: no spells found")
+    return found_spells
 
 def choose_alignment (flavor: str) :
     alignment_options = {
